5dd9ebb0436b90484173ef9a04ffb55227adccdd
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 17:50:32 -0600

    Store notes on realtime database

---
+++ src/context/NotesContext.tsx
@@ -0,0 +1,67 @@
+'use client'
+import React, { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { ref, onValue, push, set, serverTimestamp } from "firebase/database";
+
+import { database } from "@/services/firebase";
+
+type Note = {
+  id: string;
+  content: string;
+  title: string;
+  timestamp: number;
+};
+
+interface NotesContextType {
+  notes: Note[];
+  addNote: (content: string) => void;
+}
+
+const NotesContext = createContext<NotesContextType | undefined>(undefined);
+
+export const NotesProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const notesRef = ref(database, "notes/");
+    const unsubscribe = onValue(notesRef, (snapshot) => {
+      const data = snapshot.val();
+      const loadedNotes = [];
+      for (const key in data) {
+        loadedNotes.push({
+          id: key,
+          content: data[key].content,
+          title: data[key].title,
+          timestamp: data[key].timestamp,
+        });
+      }
+
+      setNotes(loadedNotes);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const addNote = (content: string, title: string = "New note") => {
+    const notesRef = ref(database, "notes/");
+    const newNoteRef = push(notesRef);
+    set(newNoteRef, {
+      content,
+      title,
+      timestamp: serverTimestamp()
+    });
+  };
+
+  return (
+    <NotesContext.Provider value={{ notes, addNote }}>
+      {children}
+    </NotesContext.Provider>
+  );
+};
+
+export const useNotes = () => {
+  const context = useContext(NotesContext);
+  if (context === undefined) {
+    throw new Error("useNotes must be used within a NotesProvider");
+  }
+  return context;
+};
