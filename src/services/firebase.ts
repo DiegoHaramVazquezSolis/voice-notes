74acfac6e5a8c6ab3d22c0a8056b3a182fa10c96
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 17:45:41 -0600

    Export firebase realtime database

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const storage = getStorage(app);
+export const database = getDatabase(app);
