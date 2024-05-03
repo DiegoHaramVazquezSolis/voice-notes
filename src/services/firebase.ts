d4298ace46019755b306ea736cc624c8107b7916
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 14:48:05 -0600

    Firebase initialized

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpPLVIayQH6q8tFUC3YUSvhKuhZckCtKg",
+  authDomain: "voice-notes-4bec3.firebaseapp.com",
+  projectId: "voice-notes-4bec3",
+  storageBucket: "voice-notes-4bec3.appspot.com",
+  messagingSenderId: "837751634692",
+  appId: "1:837751634692:web:3e80edfe3c2d3effd0ee51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
