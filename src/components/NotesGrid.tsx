424ac5c2e596d5c9c5ff6f97fb74a1fa6d12de7c
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 18:12:53 -0600

    Render a masonry grid on small or bigger screens

--- src/components/NotesGrid.tsx
+++ src/components/NotesGrid.tsx
@@ -7,20 +7,23 @@ const NotesGrid = () => {
   const { notes } = useNotes();
 
   return (
-    <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="columns-1 sm:columns-2 md:columns-3 gap-4">
       {notes.map((note) => (
-        <div key={note.id} className="flex flex-col gap-6 rounded-lg shadow-lg p-4 border-2 border-slate-800">
-          <div className="flex flex-col gap-2 h-full">
+        <div key={note.id} className="break-inside-avoid mb-4 rounded-lg shadow-lg p-4 border-2 border-slate-800">
+          <div className="flex flex-col gap-2 mb-8">
             <h2 className="text-xl font-bold">
               {note.title}
             </h2>
-            <p className="text-sm max-h-24 line-clamp-3">
+            <p className="text-sm line-clamp-3">
               {note.content}
             </p>
           </div>
-          <div className="flex flex-row justify-between self-end">
+          <div className="flex justify-end">
             <small className="text-xs">
-              {new Date(note.timestamp).toLocaleDateString()}
+              {new Date(note.timestamp).toLocaleDateString(undefined, {
+                year: 'numeric', month: 'numeric', day: 'numeric',
+                hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false
+              })}
             </small>
           </div>
         </div>
