0c6fea6666f8619b5fd2a440c74bd0e43b18750f
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 18:51:41 -0600

    Remove whitespace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { CreateNoteButton } from "@/components/CreateNoteButton";
 import { NotesGrid } from "@/components/NotesGrid";
 
 export default function HomePage() {
-
   return (
     <main className="mx-auto w-full h-screen flex flex-col items-center max-w-lg pt-16 gap-2">
       <div className="flex w-full justify-end py-2">
