4f36d1f39053fb193540631e269c737897d4480f
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 18:22:02 -0600

    Add fullwidth prop to Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,17 @@
 import React, { MouseEventHandler, ReactNode } from 'react';
 
 interface ButtonProps {
-  children: ReactNode,
-  onClick: MouseEventHandler<HTMLButtonElement>
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+  fullWidth?: boolean
 }
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, onClick, fullWidth = true }: ButtonProps) => {
+  const buttonStyle = fullWidth ? "w-full" : "";
+
   return (
     <button
-      className="flex flex-col items-center gap-4 py-2.5  rounded-md text-slate-950 bg-slate-50 hover:bg-slate-50/80 transition-colors w-full"
+      className={`flex flex-col items-center gap-4 py-2.5 px-6 rounded-md text-slate-950 bg-slate-50 hover:bg-slate-50/80 transition-colors ${buttonStyle}`}
       onClick={onClick}
     >
       {children}
