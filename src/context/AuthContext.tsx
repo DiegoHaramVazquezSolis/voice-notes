fb4e3a2521efbb5206541bdaa81f8811f4f3574d
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-04 10:13:29 -0600

    AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React, { ReactNode, createContext, useContext, useEffect, useState } from "react";
+
+import { auth } from "@/services/firebase";
+import { User, onAuthStateChanged } from "firebase/auth";
+
+interface AuthContextType {
+  user: User | null;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
