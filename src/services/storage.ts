fc174964cf78ab8871da677df1730b081effb51f
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-04 13:29:29 -0600

    Avoid get download url when possible

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -6,7 +6,17 @@ export async function uploadChunk(chunk: Blob[], streamId: string) {
   const storageRef = ref(storage, `${streamId}.webm`);
   const blob = new Blob(chunk, { type: "audio/webm" });
 
-  const { ref: chunkRef } = await uploadBytes(storageRef, blob);
+  const { ref: chunkRef, metadata } = await uploadBytes(storageRef, blob);
+
+  if (Array.isArray(metadata.downloadTokens) && metadata.downloadTokens.length > 0) {
+
+    return `https://firebasestorage.googleapis.com/v0/b/${chunkRef.bucket}/o/${chunkRef.fullPath}?alt=media&token=${metadata.downloadTokens[0]}`;
+  }
+
+  if (typeof metadata.downloadTokens === "string") {
+
+    return `https://firebasestorage.googleapis.com/v0/b/${chunkRef.bucket}/o/${chunkRef.fullPath}?alt=media&token=${metadata.downloadTokens}`;
+  }
 
   return await getDownloadURL(chunkRef);
 }
