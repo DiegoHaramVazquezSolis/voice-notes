b9d4a5c31d4d40f5e0c452ec8b72f4fd90b04e88
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 19:10:03 -0600

    Display empty list and loading notes placeholders

--- src/components/NotesGrid.tsx
+++ src/components/NotesGrid.tsx
@@ -1,10 +1,34 @@
 'use client'
-import React from 'react';
+import React from "react";
+import { LoaderCircle, Notebook } from "lucide-react";
 
-import { useNotes } from '@/context/NotesContext';
+import { useNotes } from "@/context/NotesContext";
+import { CreateNoteButton } from "@/components/CreateNoteButton";
 
 const NotesGrid = () => {
   const { notes } = useNotes();
+
+  if (!notes) {
+    return (
+      <div className="grow flex items-center">
+        <LoaderCircle
+          className="animate-spin"
+          size={48} />
+      </div>
+    );
+  }
+
+  if (notes.length === 0) {
+    return (
+      <div className="grow flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col items-center gap-4">
+          <Notebook size={48} />
+          You don&apos;t have any note yet, create your first one!
+        </div>
+        <CreateNoteButton />
+      </div>
+    );
+  }
 
   return (
     <div className="columns-1 sm:columns-2 md:columns-3 gap-4">
@@ -21,8 +45,8 @@ const NotesGrid = () => {
           <div className="flex justify-end">
             <small className="text-xs">
               {new Date(note.timestamp).toLocaleDateString(undefined, {
-                year: 'numeric', month: 'numeric', day: 'numeric',
-                hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false
+                year: "numeric", month: "numeric", day: "numeric",
+                hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false
               })}
             </small>
           </div>
