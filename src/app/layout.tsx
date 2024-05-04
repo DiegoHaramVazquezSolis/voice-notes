4ee7e650d26219cc139182ab83c572b91ab48817
DiegoHaramVazquezSolis <diegovazquezso@live.com.mx>
2024-05-03 18:51:10 -0600

    Sonner toaster implemented on RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 
 import "./globals.css";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             {children}
           </NotesProvider>
         </PermissionProvider>
+        <Toaster />
       </body>
     </html>
   );
